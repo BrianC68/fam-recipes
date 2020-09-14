40801249a94090507b758ad5a238ad27ae251446
Brian Christensen <brianc@wi.rr.com>
2020-09-14 17:12:24 -0500

    add check for is_staff

--- recipes/views.py
+++ recipes/views.py
@@ -233,7 +233,7 @@ class BrowseListView(LoginRequiredMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['contributors'] = self.user_model.objects.all().order_by('last_name')
+        context['contributors'] = self.user_model.objects.filter(is_staff=True).order_by('last_name')
         return context
 
 
